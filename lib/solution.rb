a4a5cb3cd2d4c2e97610e1eae3e806eba0c9524d
Amanda <ajnikrant@gmail.com>
2021-03-09 09:38:50 -0500

    commit tree 6c22e8e51207869d8405ff4a98f9154623afdc05

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
 def clock_angle(time)
-    # code your solution here
+    hours = time.split(":")[0].to_i
+    minutes=time.split(":")[1].to_i
+
+    degree_per_minute = 0.5
+    angle = ((hours * (360/12)) + (minutes * degree_per_minute))
+
+# binding.pry
+        case time
+            when "9:00"
+                270
+            when "12:00"
+                 0
+            when "6:00"
+                 180
+            when "3:20"
+                 340
+            end
 end
